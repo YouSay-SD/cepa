7cafd92747728f80b2ed0b74047e81fc67f6a6fd
YouSay <yousay.sd@gmail.com>
2022-03-12 13:04:18 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
-import '../styles/globals.css'
+import '../styles/sanitize.css/sanitize.css'
+import '../styles/sanitize.css/assets.css'
+import '../styles/sanitize.css/forms.css'
+import '../styles/sanitize.css/reduce-motion.css'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
