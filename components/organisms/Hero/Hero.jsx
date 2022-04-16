a474ca35ba0ee3fbe940980d3e28ef9f6dbdf461
YouSay <yousay.sd@gmail.com>
2022-04-16 19:27:30 -0300

    feat: add hero animation

--- components/organisms/Hero/Hero.jsx
+++ components/organisms/Hero/Hero.jsx
@@ -2,28 +2,32 @@ import styles from './Hero.module.scss'
 import { Container, Button, Title, P, LinkSection } from '../../atoms'
 import Image from 'next/image'
 import ReactMarkdown from 'react-markdown'
-import Link from 'next/link'
+import { useInView } from 'react-intersection-observer';
 
 const Hero = ({ title, description, image, ctaTextPrimary, ctaLinkPrimary, ctaTextSecondary }) => {
+	const { ref, inView } = useInView();
+	const classAnimPlay = inView && styles['anim-play'];
+  const classAnimation = `${styles['anim-fade-in-left']} ${classAnimPlay}`
+
   return (
-    <section className={styles.hero}>
+    <section className={styles.hero} ref={ref}>
       <Container className={styles.container}>
         <div className={styles['text-container']}>
-          <Title className={styles.title} size='xlg' color='white'>
+          <Title className={`${styles.title} ${classAnimation}`} size='xlg' color='white' h='1'>
             <ReactMarkdown>
               {title}
             </ReactMarkdown>
           </Title>
 
           {description &&
-            <P as="div" className={styles.description} color='white'>
+            <P as="div" className={`${styles.description} ${classAnimation} ${styles['anim-delay-2']}`} color='white'>
               <ReactMarkdown>
                 {description}
               </ReactMarkdown>
             </P>
           }
 
-          <div className={styles['button-container']}>
+          <div className={`${styles['button-container']} ${classAnimation} ${styles['anim-delay-3']}`}>
             {ctaLinkPrimary &&
               <Button type='outline-white' color='primary'>
                 <a href={ctaLinkPrimary} target='_blank' rel="noreferrer">{ctaTextPrimary}</a>
